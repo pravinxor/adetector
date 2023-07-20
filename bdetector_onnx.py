723ef8cfecf182c2c3dd9c88d70063f821030c09
Pravin Ramana <pravinramana25@protonmail.ch>
2023-07-20 12:24:30 -0500

    Lazy audio segmentation
    Instead of fully loading audio into memory and splitting for inference, load segments as needed using ffmpeg. This avoids extreme memory usage for large audio files at the cost of repeating calls to ffmpeg

--- bdetector_onnx.py
+++ bdetector_onnx.py
@@ -1,8 +1,7 @@
 #!/usr/bin/env python
 
 import argparse
-import colored
-from colored import stylize
+from colored import stylize, Style, Fore
 import numpy as np
 import onnx
 import onnxruntime
@@ -67,21 +66,35 @@ def chunker(seq: np.ndarray, size: int):
     return (seq[pos:pos + size] for pos in range(0, len(seq), size))
 
 
-def load_audio(file: str, sr: int):
-    cmd = [
-        'ffmpeg', '-i', file, '-f', 's16le', '-ac', '1', '-acodec',
-        'pcm_s16le', '-ar',
-        str(sr), '-'
-    ]
-    try:
-        out = subprocess.run(cmd, capture_output=True, check=True).stdout
-    except subprocess.CalledProcessError as e:
-        raise RuntimeError(f"Failed to load audio: {e.stderr.decode()}") from e
+def load_audio(file: str, sr: int, seq_len: float):
+    duration = seq_len / sr
+    position = 0.0
+    while True:
+        cmd = [
+            'ffmpeg', '-ss',
+            str(position), '-t',
+            str(duration), '-i', file, '-f', 's16le', '-ac', '1', '-acodec',
+            'pcm_s16le', '-ar',
+            str(sr), '-'
+        ]
+        try:
+            out = subprocess.run(cmd, capture_output=True, check=True).stdout
+        except subprocess.CalledProcessError:
+            break
 
-    buffer = np.frombuffer(out, np.int16).flatten().astype(np.float32)
-    buffer = buffer[:len(buffer) -
-                    len(buffer) % sample_rate]  # trim off residual
-    return buffer / (2.0**15)
+        buffer = np.frombuffer(out, np.int16).flatten().astype(np.float32)
+        buffer = buffer / (2.0**15)
+        buffer = buffer.reshape(1, -1)
+
+        if buffer.shape[1] == 0:
+            break
+        elif buffer.shape[1] < seq_len:
+            yield buffer
+            break
+        else:
+            yield buffer
+
+        position += duration
 
 
 if __name__ == '__main__':
@@ -107,7 +120,7 @@ if __name__ == '__main__':
                         metavar='b',
                         nargs='?',
                         type=int,
-                        default=sample_rate * 15 * 60)
+                        default=sample_rate * 10 * 60)
     parser.add_argument('--threshold',
                         metavar='t',
                         nargs='?',
@@ -139,19 +152,11 @@ if __name__ == '__main__':
         providers=onnxruntime.get_available_providers())
 
     for file in args.files:
-        print(stylize('Inferencing', colored.attr('bold')),
-              stylize(file, colored.fg('blue')))
-        audio = load_audio(file, sr=sample_rate)
+        print(stylize('Inferencing', Style.BOLD), stylize(file, Fore.blue))
+        audio = load_audio(file, sr=sample_rate, seq_len=args.batch_size)
         offset = 0
 
-        total_chunks = len(audio) // args.batch_size + int(
-            len(audio) % args.batch_size > 0)
-
-        for chunk in tqdm(chunker(audio, args.batch_size),
-                          total=total_chunks,
-                          leave=False):
-            chunk = chunk.reshape(1, -1)
-
+        for chunk in tqdm(audio, leave=False):
             ort_inputs = {'input': chunk}
             framewise_output = ort_session.run(['output'], ort_inputs)[0]
 
@@ -159,5 +164,3 @@ if __name__ == '__main__':
                              args.threshold, args.focus_idx, offset)
 
             offset += len(chunk[0]) / sample_rate
-            del chunk
-        del audio
