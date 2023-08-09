09f90ec04651a3464165f1543fe4df8b4ece67dc
Pravin Ramana <pravinramana25@protonmail.ch>
2023-08-08 20:17:41 -0500

    Transcode audio in nonblocking manner

--- bdetector_onnx.py
+++ bdetector_onnx.py
@@ -4,11 +4,13 @@ import argparse
 from colored import stylize, Style, Fore
 import numpy as np
 import onnx
-import onnxruntime
-from tqdm import tqdm
+import onnxruntime as ort
+import queue
 import subprocess
+import threading
+from tqdm import tqdm
 
-sample_rate = 32000
+SAMPLE_RATE = 32000
 
 
 def seconds_to_hms(seconds):
@@ -62,39 +64,48 @@ def print_timestamps(
                   threshold)
 
 
-def chunker(seq: np.ndarray, size: int):
-    return (seq[pos:pos + size] for pos in range(0, len(seq), size))
+def load_audio(file: str, sr: int, frame_count: int):
+    cmd = [
+        'ffmpeg', '-i', file, '-f', 's16le', '-ac', '1', '-acodec',
+        'pcm_s16le', '-ar',
+        str(sr), '-'
+    ]
+    ffmpeg_process = subprocess.Popen(cmd,
+                                      stdout=subprocess.PIPE,
+                                      stderr=subprocess.PIPE)
 
+    # Create a queue to store the stdout data
+    q = queue.Queue()
 
-def load_audio(file: str, sr: int, seq_len: float):
-    duration = seq_len / sr
-    position = 0.0
-    while True:
-        cmd = [
-            'ffmpeg', '-ss',
-            str(position), '-t',
-            str(duration), '-i', file, '-f', 's16le', '-ac', '1', '-acodec',
-            'pcm_s16le', '-ar',
-            str(sr), '-'
-        ]
+    # Define a function to run in a thread
+    def enqueue_output(out, q, chunk_size):
+        while True:
+            data = out.read(chunk_size)
+            if data:
+                q.put(data)
+            else:
+                break
+        out.close()
+
+    # Start a thread to read data from stdout and write it to the queue
+    t = threading.Thread(target=enqueue_output,
+                         args=(ffmpeg_process.stdout, q, frame_count * 2))
+    t.daemon = True
+    t.start()
+
+    while ffmpeg_process.poll() is None or not q.empty():
         try:
-            out = subprocess.run(cmd, capture_output=True, check=True).stdout
-        except subprocess.CalledProcessError:
-            break
+            # Read data from the queue
+            chunk = q.get(timeout=1)
+            if chunk is not None:
+                yield np.frombuffer(chunk, dtype=np.int16).reshape(
+                    1, -1).astype(np.float32) / (2.0**15)
+            else:
+                break
+        except queue.Empty:
+            pass
 
-        buffer = np.frombuffer(out, np.int16).flatten().astype(np.float32)
-        buffer = buffer / (2.0**15)
-        buffer = buffer.reshape(1, -1)
-
-        if buffer.shape[1] == 0:
-            break
-        elif buffer.shape[1] < seq_len:
-            yield buffer
-            break
-        else:
-            yield buffer
-
-        position += duration
+    _rc = ffmpeg_process.poll()
 
 
 if __name__ == '__main__':
@@ -111,26 +122,31 @@ if __name__ == '__main__':
         metavar='p',
         nargs='?',
         type=int,
-        default=100,
+        default=1 * 100,
         help=
         'the precision (in ms) of the timestamp selection process (higher is less precise)'
     )
-
-    parser.add_argument('--batch_size',
-                        metavar='b',
-                        nargs='?',
-                        type=int,
-                        default=sample_rate * 10 * 60)
-    parser.add_argument('--threshold',
-                        metavar='t',
-                        nargs='?',
-                        type=int,
-                        default=0)
+    parser.add_argument(
+        '--block_size',
+        metavar='b',
+        nargs='?',
+        type=int,
+        default=60 * 10,
+        help=
+        'the block size is the amount of seconds (of samples) to process at once. Larger sizes offer better performance, but will consume significantly more memory'
+    )
+    parser.add_argument(
+        '--threshold',
+        metavar='t',
+        nargs='?',
+        type=int,
+        default=90,
+        help='The confidence threshold for a sound to be reported')
     parser.add_argument('--focus_idx',
                         metavar='i',
+                        nargs='?',
                         type=int,
-                        help='the index of the audio class to track',
-                        required=True)
+                        help='the index of the audio class to track')
     parser.add_argument('--model',
                         metavar='m',
                         type=str,
@@ -142,25 +158,29 @@ if __name__ == '__main__':
     model = onnx.load(args.model)
     onnx.checker.check_model(model)
 
-    sess_options = onnxruntime.SessionOptions()
-    sess_options.graph_optimization_level = onnxruntime.GraphOptimizationLevel.ORT_ENABLE_ALL
-    sess_options.optimized_model_filepath = args.model
+    sess_options = ort.SessionOptions()
+    sess_options.graph_optimization_level = ort.GraphOptimizationLevel.ORT_ENABLE_ALL
+    # sess_options.optimized_model_filepath = args.model
 
-    ort_session = onnxruntime.InferenceSession(
-        args.model,
-        sess_options,
-        providers=onnxruntime.get_available_providers())
+    ort_session = ort.InferenceSession(args.model,
+                                       sess_options,
+                                       providers=ort.get_available_providers())
 
     for file in args.files:
         print(stylize('Inferencing', Style.BOLD), stylize(file, Fore.blue))
-        audio = load_audio(file, sr=sample_rate, seq_len=args.batch_size)
+        # audio = load_audio(file, sr=samplerate, seq_len=args.batch_size)
         offset = 0
 
-        for chunk in tqdm(audio, leave=False):
-            ort_inputs = {'input': chunk}
+        blocks = load_audio(file, SAMPLE_RATE, SAMPLE_RATE * args.block_size)
+
+        for block in tqdm(blocks, leave=False):
+            block_resampled = block
+
+            ort_inputs = {'input': block_resampled}
             framewise_output = ort_session.run(['output'], ort_inputs)[0]
 
-            print_timestamps(framewise_output[0], args.precision,
-                             args.threshold, args.focus_idx, offset)
+            preds = framewise_output[0]
+            print_timestamps(preds, args.precision, args.threshold,
+                             args.focus_idx, offset)
 
-            offset += len(chunk[0]) / sample_rate
+            offset += args.block_size
